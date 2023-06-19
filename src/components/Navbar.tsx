2bcddd46249e236d3241a8ad8cefa9dc8e9f5a1f
Delvoid <delvoid.dev@gmail.com>
2023-06-19 13:59:02 +0100

    Feat: user auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,30 @@
 import Link from 'next/link';
-import { Icons } from './icons';
+import { Icons } from './Icons';
 import { buttonVariants } from './ui/Button';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { UserAccountNav } from './UserAccountNav';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-      <div className="container max-w-7-xl h-full mx-autp flex items-center justify-between gap-2">
+      <div className="container max-w-7-xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href={'/'} className="flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
           <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
         </Link>
-        <Link href={'/sign-on'} className={buttonVariants()}>
-          Sign In
-        </Link>
+        {/* search bar */}
+        {/* <SearchBar /> */}
+
+        {/* actions */}
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
